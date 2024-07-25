efa17d9d5e7abe8660f2be12933319b1e9e536eb
정진우 <corosica1@gmail.com>
2024-07-25 17:33:49 +0900

    [Feat] 투표 메세지 전송 기능 구현, 투표 생성 구현(wip)

--- BanChan_FE/src/components/VoteManage/FinishedVote.tsx
+++ BanChan_FE/src/components/VoteManage/FinishedVote.tsx
@@ -1,7 +1,114 @@
 // import React from "react";
 
 const FinishedVote = () => {
-  return <div>FinishedVote</div>;
+  const vote = [
+    {
+    id:1,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:2,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:3,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:4,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:5,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:6,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:7,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:8,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:9,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+    {
+    id:10,
+    title : 'LH 7월 3주차 회의 안건 관련 투표',
+    writer : '관리자',
+    date : '24-07-18 00:00 ~ 24-07-24 23:59',
+    vote_rate : '38%'
+    },
+]
+  
+  return (
+    <>
+    <div className="flex justify-end p-5">
+            <div className="h-[56px] flex">
+
+            </div>
+        </div>
+        <table className="min-w-full bg-white border table-fixed rounded-[20px] overflow-hidden">
+          <thead>
+            <tr className=" text-slate-400">
+              <th className="px-4 py-2">번호</th>
+              <th className="px-4 py-2">제목</th>
+              <th className="px-4 py-2">작성자</th>
+              <th className="px-4 py-2">투표 기간</th>
+              <th className="px-4 py-2">투표율</th>
+            </tr>
+          </thead>
+          <tbody>
+            {vote.map((item) => (
+            <Link to="/vote/detail" key={item.id} className="contents">
+
+              <tr className=' text-center w-full border-b'>
+                    <td className="p-4">{item.id}</td>
+                    <td className="p-4">{item.title}</td>
+                    <td className="p-4">{item.writer}</td>
+                    <td className="p-4">{item.date}</td>
+                    <td className="p-4">{item.vote_rate}</td>
+                </tr>
+            </Link>
+            ))}
+            
+          </tbody>
+      </table>
+    </>
+  );
 };
 
 export default FinishedVote;
--- BanChan_FE/src/pages/VoteManagePage.tsx
+++ BanChan_FE/src/pages/VoteManagePage.tsx
@@ -1,132 +1,30 @@
 import React from 'react';
 import { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link,Outlet } from "react-router-dom";
 
 const VoteManagePage: React.FC = () => {
     const [activeTab, setActiveTab] = useState('진행중인 투표');
 
-  const vote = [
-    {
-    id:1,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:2,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:3,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:4,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:5,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:6,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:7,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:8,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:9,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-    {
-    id:10,
-    title : 'LH 7월 3주차 회의 안건 관련 투표',
-    writer : '관리자',
-    date : '24-07-18 00:00 ~ 24-07-24 23:59',
-    vote_rate : '38%'
-    },
-]
   return (
       <div className="p-8">
       <div className="">
       <nav className="flex space-x-10 ">
-          <a
-            href="#"
+          <Link
+            to="active"
             className={`flex items-center ${activeTab === '진행중인 투표' ? 'text-blue-500' : 'text-gray-600'}`}
             onClick={() => setActiveTab('진행중인 투표')}
             >
             <span className="ml-2">진행중인 투표</span>
-          </a>
-          <a
-            href="#"
+          </Link>
+          <Link
+            to="finish"
             className={`flex items-center ${activeTab === '완료된 투표' ? 'text-blue-500' : 'text-gray-600'}`}
             onClick={() => setActiveTab('완료된 투표')}
             >
             <span className="ml-2">완료된 투표</span>
-          </a>
-        </nav>
-        <div className="flex justify-end p-5">
-            <button className="mt-4  bg-blue-600/70 text-white py-2 px-4 rounded-full">
-            <Link to='/vote/create'>
-            투표 생성하기
-            </Link>
-            </button>
-        </div>
-        <table className="min-w-full bg-white border table-fixed rounded-[20px] overflow-hidden">
-          <thead>
-            <tr className=" text-slate-400">
-              <th className="px-4 py-2">번호</th>
-              <th className="px-4 py-2">제목</th>
-              <th className="px-4 py-2">작성자</th>
-              <th className="px-4 py-2">투표 기간</th>
-              <th className="px-4 py-2">투표율</th>
-            </tr>
-          </thead>
-          <tbody>
-            {vote.map((item) => (
-                <tr className=' text-center w-full border-b'>
-                    <td className="p-4">{item.id}</td>
-                    <td className="p-4">{item.title}</td>
-                    <td className="p-4">{item.writer}</td>
-                    <td className="p-4">{item.date}</td>
-                    <td className="p-4">{item.vote_rate}</td>
-                </tr>
-            ))}
-            
-          </tbody>
-        </table>
-
+          </Link>
+      </nav>
+        <Outlet />
       </div>
     </div>
   );
