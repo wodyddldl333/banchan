c0a4fb661a79661820c8178e80ccbf5750c77923
정진우 <corosica1@gmail.com>
2024-07-26 16:06:36 +0900

    [Feat] 투표 작성 투표추가 구현

--- BanChan_FE/src/components/VoteManage/VoteForm.tsx
+++ BanChan_FE/src/components/VoteManage/VoteForm.tsx
@@ -28,25 +28,23 @@ const VoteForm: React.FC<VoteFormProps> = ({ id, onDelete }) => {
 
   return (
     <div className="p-4 mb-4 border rounded shadow-md">
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl font-bold">Create a Poll</h2>
-        <button
-          onClick={() => onDelete(id)}
-          className="p-2 text-red-500 hover:text-red-700"
-        >
-          Delete Form
-        </button>
-      </div>
-      <div className="mb-4">
-        <label htmlFor={`pollTitle-${id}`} className="block text-sm font-medium text-gray-700">Poll Title</label>
-        <input
+      <div className="flex justify-between items-center mb-2">
+          <input
           id={`pollTitle-${id}`}
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
-        />
+          className="mt-1 p-2 flex-1 border border-gray-300 rounded-md"
+          required
+          />
+        <button
+          onClick={() => onDelete(id)}
+          className=" ml-2 p-2 text-red-500 hover:text-red-700"
+        >
+            X
+         </button>
       </div>
+
       {items.map((item) => (
         <div key={item.id} className="flex items-center mb-2">
           <input
@@ -54,12 +52,13 @@ const VoteForm: React.FC<VoteFormProps> = ({ id, onDelete }) => {
             value={item.text}
             onChange={(e) => handleItemChange(item.id, e.target.value)}
             className="mt-1 p-2 flex-1 border border-gray-300 rounded-md"
+            required
           />
           <button
             onClick={() => handleRemoveItem(item.id)}
             className="ml-2 p-2 text-red-500 hover:text-red-700"
           >
-            Delete
+            항목 삭제
           </button>
         </div>
       ))}
@@ -67,7 +66,7 @@ const VoteForm: React.FC<VoteFormProps> = ({ id, onDelete }) => {
         onClick={handleAddItem}
         className="mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
       >
-        Add Item
+        항목 추가
       </button>
     </div>
   );
