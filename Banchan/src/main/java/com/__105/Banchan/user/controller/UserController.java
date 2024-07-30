772ebdafb961a1fd8564e0b766b49ad997a20a88
최동호 <raspic5874@naver.com>
2024-07-30 10:41:01 +0900

    [Feat] 유저 정보 초기설정(이름,전화번호) 구현

--- Banchan/src/main/java/com/__105/Banchan/user/controller/UserController.java
+++ Banchan/src/main/java/com/__105/Banchan/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.__105.Banchan.user.controller;
 
 import com.__105.Banchan.user.domain.User;
+import com.__105.Banchan.user.dto.SignupRequestDto;
 import com.__105.Banchan.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +11,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,6 +27,14 @@ public class UserController {
     @Operation(summary = "내 정보 조회", description = "현재 로그인된 사용자의 정보를 반환합니다.")
     public ResponseEntity<User> getMyInfo() {
         User user = userService.getMyInfo();
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/setmyinfo")
+    @Operation(summary = "내 정보 초기 설정", description = "초기 가입시 입력한 정보를 반영합니다.")
+    public ResponseEntity<User> setMyInfo(@RequestBody SignupRequestDto signupRequestDto) {
+        User user = userService.getMyInfo();
+        userService.setMyInfo(user, signupRequestDto);
         return ResponseEntity.ok().body(user);
     }
 }
--- Banchan/src/main/java/com/__105/Banchan/user/service/impl/UserServiceImpl.java
+++ Banchan/src/main/java/com/__105/Banchan/user/service/impl/UserServiceImpl.java
@@ -5,9 +5,12 @@ import com.__105.Banchan.auth.jwt.JwtAuthFilter;
 import com.__105.Banchan.common.exception.CustomException;
 import com.__105.Banchan.common.exception.ErrorCode;
 import com.__105.Banchan.user.domain.User;
+import com.__105.Banchan.user.dto.SignupRequestDto;
 import com.__105.Banchan.user.repository.UserRepository;
 import com.__105.Banchan.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     private final UserRepository userRepository;
 
     @Override
@@ -25,5 +29,30 @@ public class UserServiceImpl implements UserService {
 
         return userRepository.findByEmail(userDto.getEmail())
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Override
+    public void setMyInfo(User user, SignupRequestDto signupRequestDto) {
+
+        User setUser = userRepository.findByUsername(user.getUsername())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        String phoneNumber = signupRequestDto.getPhone();
+
+        if (userRepository.findByPhone(phoneNumber).isPresent()) {
+
+            log.error("이미 존재하는 전화번호입니다.");
+
+            throw new CustomException(ErrorCode.PHONE_DUPLICATION);
+
+        }
+
+        setUser.changePhone(signupRequestDto.getPhone());
+
+        setUser.changeRealname(signupRequestDto.getRealname());
+
+        userRepository.save(setUser);
+
+        log.info("전화번호, 이름 수정 완료");
     }
 }
