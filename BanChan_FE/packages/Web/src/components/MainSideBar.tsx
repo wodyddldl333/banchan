0e6f86f4e44c67a12c68e21a153fc2b563d2c648
정진우 <corosica1@gmail.com>
2024-08-02 15:41:46 +0900

    [Env] 프론트엔드 도커 파일 수정

--- BanChan_FE/packages/Web/src/components/MainSideBar.tsx
+++ BanChan_FE/packages/Web/src/components/MainSideBar.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../../../shared/public/assets/logo.png";
+import logo from "@assets/logo.png";
 
 interface SidebarItemProps {
   icon: string;
