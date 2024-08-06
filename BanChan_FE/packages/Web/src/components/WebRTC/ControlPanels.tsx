ce8715a48b3914ccbc6f9202ebb04f885138927f
정진우 <corosica1@gmail.com>
2024-08-06 16:50:59 +0900

    [Env] 빌드 에러 처리

--- BanChan_FE/packages/Web/src/components/WebRTC/ControlPanels.tsx
+++ BanChan_FE/packages/Web/src/components/WebRTC/ControlPanels.tsx
@@ -12,89 +12,91 @@
 //   | "chat_bubble"
 //   | "notifications";
 
-const ControlPanels: React.FC<{
-  onChatToggle: () => void;
-  activeIcons: Record<IconName, boolean>;
-  handleButtonClick: (icon: IconName) => void;
-}> = ({ onChatToggle, activeIcons, handleButtonClick }) => {
-  useEffect(() => {
-    console.log(activeIcons); // activeIcons 상태가 변경될 때마다 로그를 출력
-  }, [activeIcons]);
-  return (
-    <div className="px-4 flex items-center mt-4">
-      <div className="flex space-x-8 ml-[210px]">
-        <button
-          className={`bg-gray-800 text-white flex items-center px-4 py-2 rounded-full ${
-            !activeIcons.mic ? "text-customRed" : "text-white"
-          }`}
-          onClick={() => handleButtonClick("mic")}
-        >
-          <span
-            className={`material-symbols-outlined ${
-              !activeIcons.mic ? "text-customRed" : "text-white"
-            }`}
-          >
-            {!activeIcons.mic ? "mic_off" : "mic"}
-          </span>
-          <span className="ml-2">
-            {!activeIcons.mic ? "마이크 켜기" : "마이크 끄기"}
-          </span>
-        </button>
-        <button
-          className={`bg-gray-800 text-white flex items-center px-4 py-2 rounded-full ${
-            !activeIcons.videocam ? "text-customRed" : "text-white"
-          }`}
-          onClick={() => handleButtonClick("videocam")}
-        >
-          <span
-            className={`material-symbols-outlined ${
-              !activeIcons.videocam ? "text-customRed" : "text-white"
-            }`}
-          >
-            {!activeIcons.videocam ? "videocam_off" : "videocam"}
-          </span>
-          <span className="ml-2">
-            {!activeIcons.videocam ? "비디오 켜기" : "비디오 끄기"}
-          </span>
-        </button>
-        <button
-          className="bg-gray-800 text-white flex items-center px-4 py-2 rounded-full"
-          onClick={() => handleButtonClick("screen_share")}
-        >
-          <span
-            className={`material-symbols-outlined ${
-              activeIcons.screen_share ? "text-customRed" : "text-white"
-            }`}
-          >
-            screen_share
-          </span>
-          <span className="ml-2">화면 공유</span>
-        </button>
-        <button
-          className={`bg-gray-800 text-white flex items-center px-4 py-2 rounded-full ${
-            activeIcons.headset_mic ? "text-customRed" : "text-white"
-          }`}
-          onClick={() => handleButtonClick("headset_mic")}
-        >
-          <span
-            className={`material-symbols-outlined ${
-              activeIcons.headset_mic ? "text-customRed" : "text-white"
-            }`}
-          >
-            {activeIcons.headset_mic ? "headset_off" : "headset_mic"}
-          </span>
-          <span className="ml-2">
-            {activeIcons.headset_mic ? "음소거 끄기" : "음소거 켜기"}
-          </span>
-        </button>
-        <button
-          className="bg-red-500 text-white flex items-center px-4 py-2 rounded-full"
-          onClick={() => handleButtonClick("exit_to_app")}
-        >
-          <span className={`material-symbols-outlined`}>exit_to_app</span>
-          <span className="ml-2">회의 종료하기</span>
-        </button>
-      </div>
+// 이 밑부터 활성화 된 부분입니다 2024.08.06
+// const ControlPanels: React.FC<{
+//   onChatToggle: () => void;
+//   activeIcons: Record<IconName, boolean>;
+//   handleButtonClick: (icon: IconName) => void;
+// }> = ({ onChatToggle, activeIcons, handleButtonClick }) => {
+//   useEffect(() => {
+//     console.log(activeIcons); // activeIcons 상태가 변경될 때마다 로그를 출력
+//   }, [activeIcons]);
+//   return (
+//     <div className="px-4 flex items-center mt-4">
+//       <div className="flex space-x-8 ml-[210px]">
+//         <button
+//           className={`bg-gray-800 text-white flex items-center px-4 py-2 rounded-full ${
+//             !activeIcons.mic ? "text-customRed" : "text-white"
+//           }`}
+//           onClick={() => handleButtonClick("mic")}
+//         >
+//           <span
+//             className={`material-symbols-outlined ${
+//               !activeIcons.mic ? "text-customRed" : "text-white"
+//             }`}
+//           >
+//             {!activeIcons.mic ? "mic_off" : "mic"}
+//           </span>
+//           <span className="ml-2">
+//             {!activeIcons.mic ? "마이크 켜기" : "마이크 끄기"}
+//           </span>
+//         </button>
+//         <button
+//           className={`bg-gray-800 text-white flex items-center px-4 py-2 rounded-full ${
+//             !activeIcons.videocam ? "text-customRed" : "text-white"
+//           }`}
+//           onClick={() => handleButtonClick("videocam")}
+//         >
+//           <span
+//             className={`material-symbols-outlined ${
+//               !activeIcons.videocam ? "text-customRed" : "text-white"
+//             }`}
+//           >
+//             {!activeIcons.videocam ? "videocam_off" : "videocam"}
+//           </span>
+//           <span className="ml-2">
+//             {!activeIcons.videocam ? "비디오 켜기" : "비디오 끄기"}
+//           </span>
+//         </button>
+//         <button
+//           className="bg-gray-800 text-white flex items-center px-4 py-2 rounded-full"
+//           onClick={() => handleButtonClick("screen_share")}
+//         >
+//           <span
+//             className={`material-symbols-outlined ${
+//               activeIcons.screen_share ? "text-customRed" : "text-white"
+//             }`}
+//           >
+//             screen_share
+//           </span>
+//           <span className="ml-2">화면 공유</span>
+//         </button>
+//         <button
+//           className={`bg-gray-800 text-white flex items-center px-4 py-2 rounded-full ${
+//             activeIcons.headset_mic ? "text-customRed" : "text-white"
+//           }`}
+//           onClick={() => handleButtonClick("headset_mic")}
+//         >
+//           <span
+//             className={`material-symbols-outlined ${
+//               activeIcons.headset_mic ? "text-customRed" : "text-white"
+//             }`}
+//           >
+//             {activeIcons.headset_mic ? "headset_off" : "headset_mic"}
+//           </span>
+//           <span className="ml-2">
+//             {activeIcons.headset_mic ? "음소거 끄기" : "음소거 켜기"}
+//           </span>
+//         </button>
+//         <button
+//           className="bg-red-500 text-white flex items-center px-4 py-2 rounded-full"
+//           onClick={() => handleButtonClick("exit_to_app")}
+//         >
+//           <span className={`material-symbols-outlined`}>exit_to_app</span>
+//           <span className="ml-2">회의 종료하기</span>
+//         </button>
+//       </div>
+// 여기까지 활성화 된 부분입니다 2024.08.06
 
 //       <div className="flex space-x-10 ml-[100px]">
 //         <span className="cursor-pointer material-symbols-outlined text-white">
--- BanChan_FE/packages/Web/src/pages/MeetingPage.tsx
+++ BanChan_FE/packages/Web/src/pages/MeetingPage.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
-import { OpenVidu, Session, Publisher, Subscriber } from "openvidu-browser";
-import { useParams, useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
-import ControlPanels from "../components/WebRTC/ControlPanels";
-import ThumbnailPlayer from "../components/WebRTC/ThumbnailPlayer";
-import SubscriberList from "../components/WebRTC/SubscribeList";
+// import React, { useEffect, useRef, useState, useCallback } from "react";
+// import { OpenVidu, Session, Publisher, Subscriber } from "openvidu-browser";
+// import { useParams, useLocation, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import ControlPanels from "../components/WebRTC/ControlPanels";
+// import ThumbnailPlayer from "../components/WebRTC/ThumbnailPlayer";
+// import SubscriberList from "../components/WebRTC/SubscribeList";
 
 // type IconName =
 //   | "record_voice_over"
@@ -232,217 +232,231 @@ import SubscriberList from "../components/WebRTC/SubscribeList";
 // };
 
 // export default MeetingPage;
+
+
+
+// 이 밑으로 활성화된 부분이였습니다 - 2024.08.06
+// import React from "react";
+
+// const baseUrl = import.meta.env.VITE_BASE_API_URL;
+
+// const MeetingPage: React.FC = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+//   const { id: sessionId } = useParams<{ id: string }>(); // URL 파라미터를 가져옵니다
+//   const [session, setSession] = useState<Session | null>(null);
+//   const [publisher, setPublisher] = useState<Publisher | null>(null);
+//   const [subscribers, setSubscribers] = useState<Subscriber[]>([]);
+//   const [isChatBoxVisible, setIsChatBoxVisible] = useState<boolean>(false);
+//   const [thumbnailPlayer, setThumbnailPlayer] = useState<
+//     Publisher | Subscriber | null
+//   >(null);
+
+//   const { token, roomName, date, startTime, active } = location.state as {
+//     token: string;
+//     roomName: string;
+//     date: string;
+//     startTime: string;
+//     active: boolean;
+//   };
+
+//   const subscriberStreams = useRef<Set<string>>(new Set());
+//   const [activeIcons, setActiveIcons] = useState<Record<IconName, boolean>>({
+//     record_voice_over: false,
+//     mic: false,
+//     videocam: false,
+//     screen_share: false,
+//     headset_mic: false,
+//     exit_to_app: false,
+//     book: false,
+//     group: false,
+//     chat_bubble: false,
+//     notifications: false,
+//   });
+
+//   const joinSession = useCallback(
+//     async (mySession: Session, token: string) => {
+//       try {
+//         if (!token || !sessionId) {
+//           console.error("SessionId or token is undefined");
+//           return;
+//         }
+
+//         console.log("Received sessionId:", sessionId);
+//         console.log("Received token:", token);
+
+//         // 세션에 연결
+//         await mySession.connect(token, { clientData: "Host" });
+//         console.log("Successfully connected to session");
+
+//         const OV = new OpenVidu();
+//         const publisher = OV.initPublisher(undefined, {
+//           audioSource: undefined,
+//           videoSource: undefined,
+//           publishAudio: true,
+//           publishVideo: true,
+//           resolution: "640x480",
+//           frameRate: 30,
+//           insertMode: "APPEND",
+//           mirror: false,
+//         });
+
+//         // 퍼블리셔를 세션에 추가
+//         mySession.publish(publisher);
+//         setPublisher(publisher);
+//         setThumbnailPlayer(publisher);
+
+//         setActiveIcons((prevState) => ({
+//           ...prevState,
+//           videocam: publisher.stream.videoActive,
+//           mic: publisher.stream.audioActive,
+//         }));
+
+//         console.log("Publisher added to session");
+//       } catch (error) {
+//         console.error("Error connecting to session:", error);
+//       }
+//     },
+//     [sessionId]
+//   );
+
+//   useEffect(() => {
+//     const initSession = async () => {
+//       if (!sessionId || !token) {
+//         console.error("sessionId or token is undefined");
+//         return;
+//       }
+
+//       const OV = new OpenVidu();
+//       const mySession = OV.initSession();
+
+//       const streamCreatedHandler = (event: any) => {
+//         console.log("Stream created:", event.stream.streamId);
+//         if (!subscriberStreams.current.has(event.stream.streamId)) {
+//           subscriberStreams.current.add(event.stream.streamId);
+//           const subscriber = mySession.subscribe(event.stream, undefined);
+//           setSubscribers((prevSubscribers) => [...prevSubscribers, subscriber]);
+//           console.log("Number of subscribers: ", subscribers.length + 1);
+//         }
+//       };
+
+//       const streamDestroyedHandler = (event: any) => {
+//         console.log("Stream destroyed:", event.stream.streamId);
+//         subscriberStreams.current.delete(event.stream.streamId);
+//         setSubscribers((prevSubscribers) =>
+//           prevSubscribers.filter(
+//             (subscriber) => subscriber.stream.streamId !== event.stream.streamId
+//           )
+//         );
+//         console.log("Number of subscribers: ", subscribers.length);
+//       };
+
+//       mySession.on("streamCreated", streamCreatedHandler);
+//       mySession.on("streamDestroyed", streamDestroyedHandler);
+
+//       await joinSession(mySession, token);
+//       setSession(mySession);
+//     };
+
+//     initSession();
+
+//     return () => {
+//       if (session) {
+//         session.off("streamCreated", streamCreatedHandler);
+//         session.off("streamDestroyed", streamDestroyedHandler);
+//         session.disconnect();
+//         setSubscribers([]);
+//         subscriberStreams.current.clear();
+//       }
+//     };
+//   }, [sessionId, token, joinSession]);
+
+//   const deleteSession = async (sessionId: string): Promise<void> => {
+//     try {
+//       await axios.delete(`${baseUrl}/api/session/delete/${sessionId}`, {
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: "Basic " + btoa("OPENVIDUAPP:YOUR_SECRET"),
+//         },
+//       });
+//       navigate("/meeting/reservedMeeting");
+
+//       console.log(`Session ${sessionId} deleted successfully`);
+//     } catch (error) {
+//       console.error(`Error deleting session ${sessionId}:`, error);
+//     }
+//   };
+
+//   const handleChatToggle = () => {
+//     setIsChatBoxVisible((prevState) => !prevState);
+//   };
+
+//   const handleButtonClick = (icon: IconName) => {
+//     if (icon === "chat_bubble") {
+//       handleChatToggle();
+//       setActiveIcons((prevState) => ({
+//         ...prevState,
+//         [icon]: !prevState[icon],
+//       }));
+//     } else if (icon === "exit_to_app") {
+//       if (session) session.disconnect();
+//       deleteSession(sessionId!);
+//     } else if (icon === "videocam") {
+//       if (publisher) {
+//         const newPublishVideo = !activeIcons.videocam;
+//         publisher.publishVideo(newPublishVideo);
+//         setActiveIcons((prevState) => ({
+//           ...prevState,
+//           videocam: newPublishVideo,
+//         }));
+//       }
+//     } else if (icon === "mic") {
+//       if (publisher) {
+//         const newPublishMic = !activeIcons.mic;
+//         publisher.publishAudio(newPublishMic);
+//         setActiveIcons((prevState) => ({
+//           ...prevState,
+//           mic: newPublishMic,
+//         }));
+//       }
+//     } else {
+//       setActiveIcons((prevState) => ({
+//         ...prevState,
+//         [icon]: !prevState[icon],
+//       }));
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center justify-center h-screen bg-customTextColor">
+//       <h1 className="text-2xl mb-4">회의 ID: {roomName}</h1>
+//       <div className="flex flex-col items-center">
+//         <div className="flex justify-center items-center mb-4">
+//           {thumbnailPlayer && (
+//             <ThumbnailPlayer
+//               stream={thumbnailPlayer.stream?.getMediaStream() ?? null}
+//             />
+//           )}
+//         </div>
+//         <SubscriberList subscribers={subscribers} />
+//       </div>
+//       <ControlPanels
+//         onChatToggle={handleChatToggle}
+//         activeIcons={activeIcons}
+//         handleButtonClick={handleButtonClick}
+//       />
+//     </div>
+//   );
+// };
+
+// export default MeetingPage;
+
+
+// 빌드 에러방지용 임시입니다.(삭제해주세요)
 import React from "react";
 
-const baseUrl = import.meta.env.VITE_BASE_API_URL;
-
-const MeetingPage: React.FC = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const { id: sessionId } = useParams<{ id: string }>(); // URL 파라미터를 가져옵니다
-  const [session, setSession] = useState<Session | null>(null);
-  const [publisher, setPublisher] = useState<Publisher | null>(null);
-  const [subscribers, setSubscribers] = useState<Subscriber[]>([]);
-  const [isChatBoxVisible, setIsChatBoxVisible] = useState<boolean>(false);
-  const [thumbnailPlayer, setThumbnailPlayer] = useState<
-    Publisher | Subscriber | null
-  >(null);
-
-  const { token, roomName, date, startTime, active } = location.state as {
-    token: string;
-    roomName: string;
-    date: string;
-    startTime: string;
-    active: boolean;
-  };
-
-  const subscriberStreams = useRef<Set<string>>(new Set());
-  const [activeIcons, setActiveIcons] = useState<Record<IconName, boolean>>({
-    record_voice_over: false,
-    mic: false,
-    videocam: false,
-    screen_share: false,
-    headset_mic: false,
-    exit_to_app: false,
-    book: false,
-    group: false,
-    chat_bubble: false,
-    notifications: false,
-  });
-
-  const joinSession = useCallback(
-    async (mySession: Session, token: string) => {
-      try {
-        if (!token || !sessionId) {
-          console.error("SessionId or token is undefined");
-          return;
-        }
-
-        console.log("Received sessionId:", sessionId);
-        console.log("Received token:", token);
-
-        // 세션에 연결
-        await mySession.connect(token, { clientData: "Host" });
-        console.log("Successfully connected to session");
-
-        const OV = new OpenVidu();
-        const publisher = OV.initPublisher(undefined, {
-          audioSource: undefined,
-          videoSource: undefined,
-          publishAudio: true,
-          publishVideo: true,
-          resolution: "640x480",
-          frameRate: 30,
-          insertMode: "APPEND",
-          mirror: false,
-        });
-
-        // 퍼블리셔를 세션에 추가
-        mySession.publish(publisher);
-        setPublisher(publisher);
-        setThumbnailPlayer(publisher);
-
-        setActiveIcons((prevState) => ({
-          ...prevState,
-          videocam: publisher.stream.videoActive,
-          mic: publisher.stream.audioActive,
-        }));
-
-        console.log("Publisher added to session");
-      } catch (error) {
-        console.error("Error connecting to session:", error);
-      }
-    },
-    [sessionId]
-  );
-
-  useEffect(() => {
-    const initSession = async () => {
-      if (!sessionId || !token) {
-        console.error("sessionId or token is undefined");
-        return;
-      }
-
-      const OV = new OpenVidu();
-      const mySession = OV.initSession();
-
-      const streamCreatedHandler = (event: any) => {
-        console.log("Stream created:", event.stream.streamId);
-        if (!subscriberStreams.current.has(event.stream.streamId)) {
-          subscriberStreams.current.add(event.stream.streamId);
-          const subscriber = mySession.subscribe(event.stream, undefined);
-          setSubscribers((prevSubscribers) => [...prevSubscribers, subscriber]);
-          console.log("Number of subscribers: ", subscribers.length + 1);
-        }
-      };
-
-      const streamDestroyedHandler = (event: any) => {
-        console.log("Stream destroyed:", event.stream.streamId);
-        subscriberStreams.current.delete(event.stream.streamId);
-        setSubscribers((prevSubscribers) =>
-          prevSubscribers.filter(
-            (subscriber) => subscriber.stream.streamId !== event.stream.streamId
-          )
-        );
-        console.log("Number of subscribers: ", subscribers.length);
-      };
-
-      mySession.on("streamCreated", streamCreatedHandler);
-      mySession.on("streamDestroyed", streamDestroyedHandler);
-
-      await joinSession(mySession, token);
-      setSession(mySession);
-    };
-
-    initSession();
-
-    return () => {
-      if (session) {
-        session.off("streamCreated", streamCreatedHandler);
-        session.off("streamDestroyed", streamDestroyedHandler);
-        session.disconnect();
-        setSubscribers([]);
-        subscriberStreams.current.clear();
-      }
-    };
-  }, [sessionId, token, joinSession]);
-
-  const deleteSession = async (sessionId: string): Promise<void> => {
-    try {
-      await axios.delete(`${baseUrl}/api/session/delete/${sessionId}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Basic " + btoa("OPENVIDUAPP:YOUR_SECRET"),
-        },
-      });
-      navigate("/meeting/reservedMeeting");
-
-      console.log(`Session ${sessionId} deleted successfully`);
-    } catch (error) {
-      console.error(`Error deleting session ${sessionId}:`, error);
-    }
-  };
-
-  const handleChatToggle = () => {
-    setIsChatBoxVisible((prevState) => !prevState);
-  };
-
-  const handleButtonClick = (icon: IconName) => {
-    if (icon === "chat_bubble") {
-      handleChatToggle();
-      setActiveIcons((prevState) => ({
-        ...prevState,
-        [icon]: !prevState[icon],
-      }));
-    } else if (icon === "exit_to_app") {
-      if (session) session.disconnect();
-      deleteSession(sessionId!);
-    } else if (icon === "videocam") {
-      if (publisher) {
-        const newPublishVideo = !activeIcons.videocam;
-        publisher.publishVideo(newPublishVideo);
-        setActiveIcons((prevState) => ({
-          ...prevState,
-          videocam: newPublishVideo,
-        }));
-      }
-    } else if (icon === "mic") {
-      if (publisher) {
-        const newPublishMic = !activeIcons.mic;
-        publisher.publishAudio(newPublishMic);
-        setActiveIcons((prevState) => ({
-          ...prevState,
-          mic: newPublishMic,
-        }));
-      }
-    } else {
-      setActiveIcons((prevState) => ({
-        ...prevState,
-        [icon]: !prevState[icon],
-      }));
-    }
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-customTextColor">
-      <h1 className="text-2xl mb-4">회의 ID: {roomName}</h1>
-      <div className="flex flex-col items-center">
-        <div className="flex justify-center items-center mb-4">
-          {thumbnailPlayer && (
-            <ThumbnailPlayer
-              stream={thumbnailPlayer.stream?.getMediaStream() ?? null}
-            />
-          )}
-        </div>
-        <SubscriberList subscribers={subscribers} />
-      </div>
-      <ControlPanels
-        onChatToggle={handleChatToggle}
-        activeIcons={activeIcons}
-        handleButtonClick={handleButtonClick}
-      />
-    </div>
-  );
+const ControlPanels: React.FC = () => {
+  return <div>MeetingPage</div>;
 };
 
-export default MeetingPage;
+export default ControlPanels;
