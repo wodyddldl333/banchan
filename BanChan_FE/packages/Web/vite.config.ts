2af9443a63edae907e7d176e0fa23a9bfb6476a6
김하휼 <wlwhssla1034@naver.com>
2024-08-12 17:39:57 +0900

    Commit

--- BanChan_FE/packages/Web/vite.config.ts
+++ BanChan_FE/packages/Web/vite.config.ts
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  // mode에 따른 환경 변수 로드
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), tsconfigPaths()],
-    define: {
-      "process.env": env, // 환경 변수를 define을 통해 사용
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  define: {
+    "process.env": process.env,
+  },
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
   server: {
@@ -21,19 +21,8 @@ export default defineConfig(({ mode }) => {
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://i11e105.p.ssafy.io", // 실제 API 서버 주소
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-    build: {
-      outDir: "dist",
-    },
-    base: "/", // 모든 경로에 /m을 붙이도록 설정
-  };
+  },
+  build: {
+    outDir: "dist",
+  },
 });
-
