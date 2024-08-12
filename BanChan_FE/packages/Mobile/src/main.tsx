4296f3c06f694ed3c0f5f4c444db7a2f4187164c
정진우 <corosica1@gmail.com>
2024-08-12 14:25:47 +0900

    [Feat] 카카오 로그인 구현

--- BanChan_FE/packages/Mobile/src/main.tsx
+++ BanChan_FE/packages/Mobile/src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { CookiesProvider } from "react-cookie";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </React.StrictMode>
 );
