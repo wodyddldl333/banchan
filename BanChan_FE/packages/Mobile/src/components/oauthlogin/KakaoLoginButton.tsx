75d912396f32ccbf801359a91d81f92169b976b1
윤동환 <sapa114605@naver.com>
2024-08-12 10:16:35 +0900

    [Fix]로그인 버튼 css 수정

--- BanChan_FE/packages/Mobile/src/components/oauthlogin/KakaoLoginButton.tsx
+++ BanChan_FE/packages/Mobile/src/components/oauthlogin/KakaoLoginButton.tsx
@@ -12,13 +12,13 @@ const KakaoLoginButton: React.FC = () => {
   return (
     <button
       onClick={handleKakaoLogin}
-      className="bg-yellow-400 text-black py-3.5 px-20 rounded-2xl flex items-center justify-center mx-4"
+      className="bg-yellow-400 text-black py-4 px-6 rounded-lg flex items-center justify-center w-full mb-4"  // 버튼 크기 조정
     >
-      <div className="w-[200px] flex items-center justify-center">
+      <div className="flex items-center justify-center">
         <img
           src="../Mobile/src/assets/kakao_logo.png"
           alt="KakaoTalk"
-          className="w-6 h-6 mr-2"
+          className="w-6 h-6 mr-2"  // 이미지 크기 유지
         />
         <span className="text-center">카카오로 계속하기</span>
       </div>
